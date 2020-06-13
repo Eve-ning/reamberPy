e79cad9dbe5043f4dbdcca9c761205af456cace0
Evening <johnchangqi@hotmail.com>
2020-06-13 20:19:06 +0800

    Add spacing between class specific funcs

--- reamber/algorithms/analysis/describe/meta.py
+++ reamber/algorithms/analysis/describe/meta.py
@@ -18,8 +18,10 @@ def mapMetadata(m, s, unicode=True) -> str:
     if isinstance(m, OsuMapObj):
         if unicode: return formatting(m.artistUnicode, m.titleUnicode, m.version)
         else: return formatting(m.artist, m.title, m.version)
+
     elif isinstance(m, QuaMapObj):
         return formatting(m.artist, m.title, m.difficultyName)
+
     elif isinstance(m, SMMapObj) and isinstance(s, SMMapSetObj):
         if unicode: return formatting(s.artist if len(s.artist.strip()) > 0 else s.artistTranslit,
                                       s.title if len(s.title.strip()) > 0 else s.titleTranslit,
@@ -27,10 +29,12 @@ def mapMetadata(m, s, unicode=True) -> str:
         else: return formatting(s.artistTranslit if len(s.artistTranslit.strip()) > 0 else s.artist,
                                 s.titleTranslit if len(s.titleTranslit.strip()) > 0 else s.title,
                                 m.difficulty)
+
     elif isinstance(m, O2JMapObj) and isinstance(s, O2JMapSetObj):
         try:
             return formatting(s.artist.strip(), s.title, f"Level {s.level[s.maps.index(m)]}")
         except IndexError:
             return formatting(s.artist, s.title, "Cannot determine level")
+        
     else:
         raise NotImplementedError
