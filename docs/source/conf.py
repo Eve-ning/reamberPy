98676ae9fae125a3a5f5be89ac6d260749332e12
Evening <johnchangqi@hotmail.com>
2020-06-17 13:29:47 +0800

    Update to 0.0.13 for minifixes

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -21,7 +21,7 @@ copyright = '2020, Eve-ning'
 author = 'Eve-ning'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.12'
+release = '0.0.13'
 
 # -- General configuration ---------------------------------------------------
 
