e03180987a5dcc951a6408ff2bee6915deb1b416
Evening <johnchangqi@hotmail.com>
2020-06-20 13:19:06 +0800

    Reroute activity to be more generic

--- reamber/algorithms/analysis/bpm/bpmActivity.py
+++ reamber/algorithms/analysis/bpm/bpmActivity.py
@@ -1,6 +1,8 @@
 from reamber.base.BpmObj import BpmObj
 from reamber.base.MapObj import MapObj
+from reamber.algorithms.analysis.generic.activity import activity
 from typing import Tuple, List
+
 
 def bpmActivity(m: MapObj) -> List[Tuple[BpmObj, float]]:
     """ Calculates how long the Bpm is active
@@ -16,18 +18,7 @@ def bpmActivity(m: MapObj) -> List[Tuple[BpmObj, float]]:
 
     :return A List of Tuples in the format [(BPMPoint, Activity In ms), ...]
     """
-    last = m.notes.lastOffset()
 
-    # Describes the BPM and Length of it active
-    # e.g. [(120.0, 2000<ms>), (180.0, 1000<ms>), ...]
-    bpmLen: List[Tuple[BpmObj, float]] = []
-
-    bpmRev = m.bpms.sorted()
-    reversed(bpmRev)
-    for bpm in bpmRev:
-        if bpm.offset >= last:
-            bpmLen.append((bpm, 0.0))  # If the BPM doesn't cover any notes it is inactive
-        else:
-            bpmLen.append((bpm, last - bpm.offset))
-            last = bpm.offset
-    return bpmLen
+    # Guaranteed to return BpmObj.
+    # noinspection PyTypeChecker
+    return activity(m.bpms, m.notes.lastOffset())
---
+++ reamber/algorithms/analysis/generic/activity.py
@@ -0,0 +1,33 @@
+from reamber.base.BpmObj import BpmObj
+from reamber.base.lists.TimedList import TimedList
+from reamber.base.TimedObj import TimedObj
+from typing import Tuple, List
+
+def activity(tl: TimedList, lastOffset:float = None) -> List[Tuple[TimedObj, float]]:
+    """ Calculates how long the Bpm is active
+
+    For example:
+
+    The algorithm calculates this::
+
+        SEC 1   2   3   4   5   6   7   8   9
+        BPM 100 ------> 200 --> 300 -------->
+
+    returns [(TimedObj<100>, 3000), (TimedObj<200>, 2000), (TimedObj<300>, 3000)]
+
+    :return A List of Tuples in the format [(BPMPoint, Activity In ms), ...]
+    """
+
+    if lastOffset is None: lastOffset = tl.lastOffset()
+
+    # Describes the BPM and Length of it active
+    # e.g. [(120.0, 2000<ms>), (180.0, 1000<ms>), ...]
+    tlLen: List[Tuple[BpmObj, float]] = []
+
+    for obj in tl.sorted(reverse=True).data():
+        if obj.offset >= lastOffset:
+            tlLen.append((obj, 0.0))  # If the BPM doesn't cover any notes it is inactive
+        else:
+            tlLen.append((obj, lastOffset - obj.offset))
+            lastOffset = obj.offset
+    return tlLen
