cfb64d81d36673d1a3ebbb9c351747cc91232b1c
Evening <johnchangqi@hotmail.com>
2020-06-21 16:51:20 +0800

    Fix issue with scrollSpeed algorithm not allowing float as input

--- reamber/algorithms/analysis/bpm/scrollSpeed.py
+++ reamber/algorithms/analysis/bpm/scrollSpeed.py
@@ -20,16 +20,17 @@ def scrollSpeed(m: QuaMapObj, centerBpm: float = None) -> List[Dict[str, float]]
     e.g. if BPM == 200.0 and CenterBPM == 100.0, it'll return {'offset': X, 'speed': 2.0}
 
     :param m: The Map Object
-    :param centerBpm: The bpm to zero calculations on. If None, it'll calculate the bpm that lasts the longest
+    :param centerBpm: The bpm to zero calculations on. If None, it'll calculate the bpm that lasts the longest \
+        May not be reliable, recommended to manually input if needed.
     :return: Returns a list dict of keys offset and speed
     """
 
     # This automatically calculates the center BPM
     # Bpm Activity implicitly sorts
-    if centerBpm is None: centerBpm = bpmActivity(m)[-1]
+    if centerBpm is None: centerBpm = bpmActivity(m)[-1][0].bpm
 
     if isinstance(m, SMMapObj) or isinstance(m, O2JMapObj):  # SM doesn't have SV
-        return [dict(offset=bpm.offset, speed=bpm.bpm/centerBpm[0].bpm) for bpm in m.bpms]
+        return [dict(offset=bpm.offset, speed=bpm.bpm/centerBpm) for bpm in m.bpms]
 
     svPairs = [(offset, multiplier) for offset, multiplier in zip(m.svs.sorted().offsets(), m.svs.multipliers())]
     bpmPairs = [(offset, bpm) for offset, bpm in zip(m.bpms.offsets(), m.bpms.bpms())]
@@ -48,6 +49,6 @@ def scrollSpeed(m: QuaMapObj, centerBpm: float = None) -> List[Dict[str, float]]
                 nextBpmOffset = bpmPairs[currBpmIter][0]
             else:
                 nextBpmOffset = None
-        speedList.append(dict(offset=offset, speed=bpmPairs[currBpmIter][1] * sv / centerBpm[0].bpm))
+        speedList.append(dict(offset=offset, speed=bpmPairs[currBpmIter][1] * sv / centerBpm))
 
     return speedList
