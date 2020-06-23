9aa8659902458579facc2199e497a1a56f0e3565
Evening <johnchangqi@hotmail.com>
2020-06-24 06:18:50 +0800

    Optimize algorithm3 a bit and depr algorithm2

--- reamber/algorithms/generate/sv/generators/svOsuMeasureLine.py
+++ reamber/algorithms/generate/sv/generators/svOsuMeasureLine.py
@@ -17,11 +17,15 @@ def svOsuMeasureLine(firstOffset: float,
                      teleportBpm: float = 999999,
                      stopBpm: float = 0.001,
                      fillBpm: float = 999999) -> SvPkg:
-    """ Generates Measure Line movement for osu! maps.
+    """ Generates Measure Line movement for osu! maps. Version 1
+
+    Uses flickering to support multi functions. It's the more stable version.
 
     Could be used for other VSRGs but if they support negative Scroll then it could be much easier.
 
-    Regardless, it'll returns a Sequence.
+    Sequence::
+
+        T_S___F,T_S___F,T_S___F,...
 
     :param firstOffset: The first Offset to start the function (x = 0)
     :param lastOffset: The last Offset to end the function (x = 1)
@@ -33,7 +37,6 @@ def svOsuMeasureLine(firstOffset: float,
     :param fillBpm: The bpm to use to fill such that the sequence ends on lastOffset. None for no fill.
     """
 
-    # T S___F,T S___F,...
     msecPerFrame = 4 + paddingSize
 
     duration = lastOffset - firstOffset
@@ -57,79 +60,25 @@ def svOsuMeasureLine(firstOffset: float,
 
     return pkgs
 
-
 def svOsuMeasureLine2(firstOffset: float,
-                      lastOffset: float,
-                      funcs: List[Callable[[float], float]],
-                      paddingSize: int = 10,
-                      teleportBpm: float = 999999,
-                      stopBpm: float = 0.000001,
-                      fillBpm: float = 999999) -> SvPkg:
-    """ Generates Measure Line movement for osu! maps.
-
-    Could be used for other VSRGs but if they support negative Scroll then it could be much easier.
-
-    Regardless, it'll returns a Sequence.
-
-    :param firstOffset: The first Offset to start the function (x = 0)
-    :param lastOffset: The last Offset to end the function (x = 1)
-    :param funcs: The functions to use. 0 <= x <= 1 will be called, expecting a BPM as an output. \
-        The more functions you have, the "laggier" it will be.
-    :param paddingSize: The size of the padding, the larger the value, the lower the FPS
-    :param teleportBpm: The bpm value for teleporting Bpms.
-    :param stopBpm: The bpm value for stop Bpms. Cannot be 0.
-    :param fillBpm: The bpm to use to fill such that the sequence ends on lastOffset. None for no fill.
-    """
-
-    # T S___F S___F,T S___F S___F,T S___F S___F, ...
-    msecPerFrame = 2 + (paddingSize + 2) * len(funcs) + 1
-
-    duration = lastOffset - firstOffset
-    frameCount = int(duration / msecPerFrame)
-
-    # Append a y = 0 to get diff on first func
-    funcs = [lambda x: 0, *funcs]
-    funcDiff = []
-
-    for funcI in range(len(funcs) - 1):  # -1 due to the appended y = 0
-        def f(x):
-            sort = sorted([g(x) for g in funcs])
-            return [g2 - g1 for g1, g2 in zip(sort[:-1], sort[1:])][funcI]
-        funcDiff.append(deepcopy(f))
-
-    funcSeq = [teleportBpm, None, stopBpm, *[None for _ in range(paddingSize)], funcDiff[0]]
-
-    for func in funcDiff[1:]: funcSeq.extend([None, stopBpm, *[None for _ in range(paddingSize)], func])
-
-    pkg = svFuncSequencer(funcs=funcSeq,
-                          offsets=1,
-                          repeats=frameCount,
-                          repeatGap=1,
-                          startX=0,
-                          endX=1)
-
-    pkg = SvPkg(map(lambda x: x.addOffset(firstOffset), pkg))
-
-    # Fill missing ending to fit to lastOffset
-    if fillBpm is not None:
-        seqLastOffset = firstOffset + frameCount * msecPerFrame
-        pkg.append(SvSequence([(offset, fillBpm) for offset in range(int(seqLastOffset),
-                                                                    int(lastOffset) + 1)]))
-
-    return pkg
-
-def svOsuMeasureLine3(firstOffset: float,
                       lastOffset: float,
                       funcs: List[Callable[[float], float]],
                       paddingSize: int = 10,
                       teleportBpm: float = 99999999,
                       stopBpm: float = 0.000000001,
                       fillBpm: float = 99999999) -> SvPkg:
-    """ Generates Measure Line movement for osu! maps.
+    """ Generates Measure Line movement for osu! maps. Version 2.
+
+    This is a beta function for svOsuMeasureLine, it may or may not work as expected.
+
+    This handles multi functions a little bit better by stacking them in a single frame instead of flickering through
+    them.
 
     Could be used for other VSRGs but if they support negative Scroll then it could be much easier.
 
-    Regardless, it'll returns a Sequence.
+    Sequence::
+
+        S____F_F_F..._T_,S____F_F_F..._T_,...
 
     :param firstOffset: The first Offset to start the function (x = 0)
     :param lastOffset: The last Offset to end the function (x = 1)
@@ -154,10 +103,9 @@ def svOsuMeasureLine3(firstOffset: float,
         funcDiff.append(deepcopy(f))
 
     funcSeq = []
-
     funcSeq.extend([stopBpm, *[None for _ in range(paddingSize)], funcDiff[0]])
     for func in funcDiff[1:]: funcSeq.extend([None, func])
-    funcSeq.extend([None, teleportBpm, None])
+    funcSeq.extend([None, stopBpm, None, teleportBpm])
 
     msecPerFrame = len(funcSeq)
 
@@ -167,7 +115,7 @@ def svOsuMeasureLine3(firstOffset: float,
     pkg = svFuncSequencer(funcs=funcSeq,
                           offsets=1,
                           repeats=frameCount,
-                          repeatGap=2,
+                          repeatGap=1,
                           startX=0,
                           endX=1)
 
