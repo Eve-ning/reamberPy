ea027be853be0ffb7d6576074d278c2e0abccd76
Evening <johnchangqi@hotmail.com>
2020-07-01 02:29:36 +0800

    Drop MA

--- reamber/algorithms/analysis/mapAnalytics/cls/MAOsu.py
+++
@@ -1,9 +0,0 @@
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from reamber.algorithms.analysis.mapAnalytics.MapAnalytics import MapAnalytics
-
-class MAOsu:
-    def __init__(self, ma: MapAnalytics): self.ma = ma
-
-    def sv(self): pass
--- reamber/algorithms/analysis/mapAnalytics/cls/parts/MAHit.py
+++
@@ -1,5 +0,0 @@
-from typing import TYPE_CHECKING
-
-if TYPE_CHECKING:
-    from reamber.algorithms.analysis.mapAnalytics.MapAnalytics import MapAnalytics
-
