195bda6e769b8883c1e974cd1a03c535bacfdef5
Evening <johnchangqi@hotmail.com>
2020-07-04 23:56:21 +0800

    Add AvoidJack and ExcludeMarked for grouping

--- reamber/algorithms/pattern/PtnPkg.py
+++ reamber/algorithms/pattern/PtnPkg.py
@@ -3,24 +3,26 @@ from __future__ import annotations
 from reamber.algorithms.pattern.PtnNote import PtnNote
 from typing import List
 from reamber.base.lists.notes.NoteList import NoteList
+from reamber.base.lists.NotePkg import NotePkg
 from reamber.algorithms.pattern.PtnGroup import PtnGroup
 import numpy as np
 from copy import deepcopy
 
 
 class PtnPkg:
-    def __init__(self, list_: NoteList or np.ndarray = None):
-        self.dt = np.dtype([('column', np.int8), ('offset', np.int32), ('confidence', np.float_)])
-        if list_ is None: self.data = np.empty(0, dtype=self.dt)
-        elif isinstance(list_, NoteList):
-            self.data = np.empty(len(list_), dtype=self.dt)
-            self.data['column'] = list_.columns()
-            self.data['offset'] = list_.offsets()
-            self.data['confidence'] = 1.0
-        elif isinstance(list_, np.ndarray):
-            assert list_.dtype == self.dt, "Mismatch of Data Type, it must be " \
-                                           "([('column', np.int8), ('offset', np.int32), ('confidence', np.float_)])"
-            self.data = list_
+    def __init__(self, lis_:NotePkg):
+        self.dt = np.dtype([('column', np.int8), ('offset', np.float_), ('confidence', np.float_)])
+        if lis_ is None:
+            self.data = np.empty(0, dtype=self.dt)
+            return
+
+        self.data = np.empty(lis_.objCount(), dtype=self.dt)
+        # noinspection PyTypeChecker
+        lisCopy = [obj for i in lis_.deepcopy().data().values() for obj in i]
+        lisCopy.sort(key=lambda x: x.offset)
+        self.data['column'] = [i.column for i in lisCopy]
+        self.data['offset'] = [i.offset for i in lisCopy]
+        self.data['confidence'] = 1.0
 
     def __len__(self):
         return len(self.data)
@@ -31,7 +33,8 @@ class PtnPkg:
     def empty(self, length: int):
         return np.empty(length, dtype=self.dt)
 
-    def group(self, vwindow: float = 50.0, hwindow:None or int = None) -> List[np.ndarray]:
+    def group(self, vwindow: float = 50.0, hwindow:None or int = None, avoidJack=True,
+              excludeMarked=True) -> List[np.ndarray]:
         """ Groups the package horizontally and vertically, returns a list of groups
 
         Let's say we want to group with the parameters
@@ -73,7 +76,13 @@ class PtnPkg:
 
         :param vwindow: The Vertical Window to check (Offsets)
         :param hwindow: The Horizontal Window to check (Columns). If none, all columns will be grouped.
+        :param avoidJack: If True, a group will never have duplicate columns.
+        :param excludeMarked: If True, any note that is already grouped will never be grouped again. If False, notes\
+            that aren't grouped will be used as reference points and may include marked objects.
         """
+        assert vwindow >= 0, "VWindow cannot be negative"
+        assert hwindow is None or hwindow >= 0, "HWindow cannot be negative, use None to group all columns available."
+
         groupedArr = np.zeros(len(self), dtype=np.bool_)
 
         grps = []
@@ -87,17 +96,33 @@ class PtnPkg:
             if vwindow >= 0:
                 # e.g. searchsorted([0,1,2,6,7,9], 3) -> 2
                 # From this we can find the indexes where the groups are.
-                left = np.searchsorted(self.data['offset'], note['offset'], side='left')
-                right = np.searchsorted(self.data['offset'], note['offset'] + vwindow, side='right')
+                left = np.searchsorted(data['offset'], note['offset'], side='left')
+                right = np.searchsorted(data['offset'], note['offset'] + vwindow, side='right')
                 vmask = np.zeros(len(data), np.bool_)
-                vmask[left:right] = 1
+                if avoidJack:
+                    # The r-hand checks if in the left-right range, if the column mismatches.
+                    # We only want mismatched columns if we avoid jack
+                    # e.g. [0, 1, 2]
+                    cols = data['column'][left:right]
+
+                    unqCols = np.unique(cols)
+                    # This finds the first occurrences of each unique column
+                    indCols = np.array([np.where(cols == col)[0][0] for col in unqCols])
+
+                    # Add left, because the where search is relative
+                    vmask[indCols+left] = True
+                else:
+                    vmask[left:right] = True
                 mask = np.bitwise_and(mask, vmask)
 
             # Filter hwindow
-            if hwindow >= 0:
+            if hwindow is not None:
                 hmask = np.zeros(len(data), np.bool_)
                 hmask[abs(note['column'] - data['column']) <= hwindow] = 1
                 mask = np.bitwise_and(mask, hmask)
+
+            if excludeMarked:
+                mask = np.bitwise_and(~groupedArr, mask)
 
             # Depending on if we want to repeat h selections, we mark differently.
             groupedArr = np.bitwise_or(groupedArr, mask)
