82eb8e1cd4efb816b0840479d1d4bb9062170d8e
Evening <johnchangqi@hotmail.com>
2020-07-08 05:38:39 +0800

    Add assertions to test

--- tests/test/algorithms/pattern/filter/test.py
+++ tests/test/algorithms/pattern/filter/test.py
@@ -12,13 +12,14 @@ class TestFilter(unittest.TestCase):
         b = PtnFilterChord.create(sizes=[[2, 2, 2], [3, 3, 3]], keys=4,
                                   method=PtnFilterChord.Method.AND_LOWER | PtnFilterChord.Method.ANY_ORDER)
         c = a & b
-        print(c.ar)
+        self.assertEqual(c.ar.shape, (27,3))
 
     def test_combo(self):
         c = PtnFilterCombo.create(cols=[[0,0,0], [1,1,1]], keys=4)
         d = PtnFilterCombo.create(cols=[[0,0,0], [2,2,2]], keys=4)
         e = c | d
-        print(e.ar)
+        self.assertEqual(e.ar.shape, (3,3))
+
 
 if __name__ == '__main__':
     unittest.main()
