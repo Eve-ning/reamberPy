6a4fa99a17d6f3fa50dfa33e53c0bb805e1da674
Evening <johnchangqi@hotmail.com>
2020-07-09 22:37:04 +0800

    Deprecate unused PtnGroup and PtnNote

--- reamber/algorithms/pattern/PtnNote.py
+++
@@ -1,8 +0,0 @@
-from reamber.base.Note import Note
-from dataclasses import dataclass
-
-
-@dataclass
-class PtnNote(Note):
-    """ Just a subclass to separate the interfaces. """
-    confidence: float = 1.0
