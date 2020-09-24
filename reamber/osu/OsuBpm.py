6371d07758c33257986a69ed89aacec762a777ab
Evening <johnchangqi@hotmail.com>
2020-09-24 09:55:37 +0800

    Add safe reading and loading for osu

--- reamber/osu/OsuBpm.py
+++ reamber/osu/OsuBpm.py
@@ -21,8 +21,12 @@ class OsuBpm(OsuTimingPointMeta, Bpm):
         return 60000.0 / value
 
     @staticmethod
-    def readString(s: str) -> OsuBpm or None:
-        """ Reads a single line under the [TimingPoints] Label. This must explicitly be a BPM Point. """
+    def readString(s: str, safe: bool = True) -> OsuBpm or None:
+        """ Reads a single line under the [TimingPoints] Label. This must explicitly be a BPM Point.
+
+        :param s: String to read
+        :param safe: Whether to clip on bad input, e.g. Division By Zero
+        """
         if s.isspace(): return None
 
         sComma = s.split(",")
@@ -31,7 +35,11 @@ class OsuBpm(OsuTimingPointMeta, Bpm):
         this = OsuBpm()
         assert sComma[6] == '1', "Unexpected SV Object in OsuBpm."
         this.offset = float(sComma[0])
-        this.bpm = OsuBpm.codeToValue(float(sComma[1]))
+        try:
+            this.bpm = OsuBpm.codeToValue(float(sComma[1]))
+        except ZeroDivisionError:
+            if safe: this.bpm = MAX_BPM
+            else: raise ZeroDivisionError("Attempted to load code == 0, leading to Div By Zero")
         this.metronome = int(sComma[2])
         this.sampleSet = int(sComma[3])
         this.sampleSetIndex = int(sComma[4])
@@ -40,8 +48,17 @@ class OsuBpm(OsuTimingPointMeta, Bpm):
 
         return this
 
-    def writeString(self) -> str:
-        """ Exports a .osu writable string """
-        return f"{self.offset},{self.valueToCode(self.bpm)}," \
+    def writeString(self, safe: bool = True) -> str:
+        """ Exports a .osu writable string
+
+        :param safe: Whether to clip on bad output, e.g. Division By Zero
+        """
+        try:
+            code = self.valueToCode(self.bpm)
+        except ZeroDivisionError:
+            if safe: code = MIN_BPM
+            else: raise ZeroDivisionError("Attempted to load value == 0, leading to Div By Zero")
+
+        return f"{self.offset},{code}," \
                f"{self.metronome},{self.sampleSet}," \
                f"{self.sampleSetIndex},{self.volume},{1},{int(self.kiai)}"
