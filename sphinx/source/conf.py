ce0a2e8f80466e45e44d016157a36f9364c54cbc
Evening <johnchangqi@hotmail.com>
2021-06-19 22:35:54 +0800

    Update the versions

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Eve-ning'
 author = 'Eve-ning'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.19a'
+release = '0.0.20'
 
 # -- General configuration ---------------------------------------------------
 
