42b2b8d1fac78d32e86dcd6e5dda91ec223baa05
Evening <johnchangqi@hotmail.com>
2021-07-07 15:11:39 +0800

    Change test as force measure is unstable

--- tests/test/timing/test.py
+++ tests/test/timing/test.py
@@ -28,14 +28,22 @@ class TestTimingMap(unittest.TestCase):
                                     bpm_changes_snap=bpm_changes_snap)
         self.assertEqual(
             [BpmChange(bpm=200, beats_per_measure=Fraction(4, 1), offset=100, measure=0, beat=0, slot=Fraction(0, 1)),
-            BpmChange(bpm=200, beats_per_measure=Fraction(3, 1), offset=1300.0, measure=1, beat=0, slot=Fraction(0, 1)),
-            BpmChange(bpm=300, beats_per_measure=Fraction(3, 1), offset=2200.0, measure=2, beat=0, slot=Fraction(0, 1)),
-            BpmChange(bpm=300, beats_per_measure=Fraction(1, 1), offset=2800.0, measure=3, beat=0, slot=Fraction(0, 1)),
-            BpmChange(bpm=200, beats_per_measure=Fraction(2, 1), offset=3000.0, measure=4, beat=0, slot=Fraction(0, 1)),
-            BpmChange(bpm=300, beats_per_measure=Fraction(1, 1), offset=3600.0, measure=5, beat=0, slot=Fraction(0, 1)),
-            BpmChange(bpm=200, beats_per_measure=Fraction(3, 2), offset=3800.0, measure=6, beat=0, slot=Fraction(0, 1)),
-            BpmChange(bpm=100, beats_per_measure=Fraction(5, 2), offset=4250.0, measure=7, beat=0, slot=Fraction(0, 1)),
-            BpmChange(bpm=300, beats_per_measure=Fraction(3, 1), offset=5750.0, measure=8, beat=0, slot=Fraction(0, 1))],
+             BpmChange(bpm=200, beats_per_measure=Fraction(3, 1), offset=1300.0, measure=1, beat=0,
+                       slot=Fraction(0, 1)),
+             BpmChange(bpm=300, beats_per_measure=Fraction(3, 1), offset=2200.0, measure=2, beat=0,
+                       slot=Fraction(0, 1)),
+             BpmChange(bpm=300, beats_per_measure=Fraction(4, 1), offset=2800.0, measure=3, beat=0,
+                       slot=Fraction(0, 1)),
+             BpmChange(bpm=200, beats_per_measure=Fraction(4, 1), offset=3000.0, measure=3, beat=1,
+                       slot=Fraction(0, 1)),
+             BpmChange(bpm=300, beats_per_measure=Fraction(4, 1), offset=3600.0, measure=3, beat=3,
+                       slot=Fraction(0, 1)),
+             BpmChange(bpm=200, beats_per_measure=Fraction(4, 1), offset=3800.0, measure=4, beat=0,
+                       slot=Fraction(0, 1)),
+             BpmChange(bpm=100, beats_per_measure=Fraction(4, 1), offset=4250.0, measure=4, beat=1,
+                       slot=Fraction(1, 2)),
+             BpmChange(bpm=300, beats_per_measure=Fraction(3, 1), offset=5750.0, measure=5, beat=0,
+                       slot=Fraction(0, 1))],
             tm.bpm_changes)
 
     # @profile
