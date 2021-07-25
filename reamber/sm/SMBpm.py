d177b45f05fa7896a4118160c3e16c33f2d57b50
Evening <johnchangqi@hotmail.com>
2021-07-25 17:10:46 +0800

    Add common beat & mbs (measure, beat, slot) conversion

--- reamber/sm/SMBpm.py
+++ reamber/sm/SMBpm.py
@@ -2,4 +2,11 @@ from reamber.base.Bpm import Bpm
 
 
 class SMBpm(Bpm):
-    ...
+    @staticmethod
+    def beat_to_mbs(beats: float):
+        """ Converts beats to measure, beats and slot """
+        return int(beats) // 4, int(beats) % 4, beats % 1
+
+    @staticmethod
+    def mbs_to_beat(measure: int, beat: int, slot: float):
+        return measure * 4 + beat + slot
