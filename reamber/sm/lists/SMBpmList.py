33f636d9c89ff006ecb6a69f2c979a4430aeb3a3
Evening <johnchangqi@hotmail.com>
2021-08-06 21:52:21 +0800

    Override reseat of BPMList for SM due to 4 beat format

--- reamber/sm/lists/SMBpmList.py
+++ reamber/sm/lists/SMBpmList.py
@@ -7,4 +7,13 @@ from reamber.sm import SMBpm
 
 @list_props(SMBpm)
 class SMBpmList(BpmList[SMBpm]):
-    ...
+
+    # TODO: Important thing to fix if we want correct snaps.
+    #  Will have to dynamically create bpm slots to avoid weird bpms
+    def reseat(self, item_class=SMBpm):
+        """ Force Reseats the BPM List such that metronomes are always 4. """
+        bpms = super().reseat(item_class)
+        bpms.bpm /= bpms.metronome / SMBpm.DEFAULT_BEATS_PER_MEASURE
+        bpms.metronome = 4.0
+        return bpms
+
