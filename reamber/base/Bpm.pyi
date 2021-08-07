99eefe871d0ec7e42a669f8dbd703b13c5adc7f9
Evening <johnchangqi@hotmail.com>
2021-08-07 20:36:48 +0800

    Deprecate old bandaid solution for TimingMap

--- reamber/base/Bpm.pyi
+++ reamber/base/Bpm.pyi
@@ -1,12 +1,6 @@
 from __future__ import annotations
 
-from typing import List, Union, TYPE_CHECKING
-
 from reamber.base.Timed import Timed
-
-if TYPE_CHECKING:
-    from reamber.base.lists.TimedList import TimedList
-    from reamber.base.lists.BpmList import BpmList
 
 
 class Bpm(Timed):
@@ -25,14 +19,3 @@ class Bpm(Timed):
     def metronome(self) -> float: ...
     @metronome.setter
     def metronome(self, val) -> None: ...
-    def beat(self, bpms: List[Bpm]): ...
-    @staticmethod
-    def snap_exact(offsets: List[float], bpms: List[Bpm], snap_precision: int = 64): ...
-    @staticmethod
-    def get_beats(offsets: Union[List[float], List[Timed], float, TimedList],
-                  bpms: 'BpmList') -> List[float]: ...
-    @staticmethod
-    def align_bpms(bpms: 'BpmList',
-                   BEAT_ERROR_THRESHOLD: float = 0.001,
-                   BEAT_CORRECTION_FACTOR: float = 5.0) -> List[Bpm]: ...
-
