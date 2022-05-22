6f9351d73804c1e077a6fd909d1d60ecc1282490
Evening <johnchangqi@hotmail.com>
2022-05-22 13:39:45 +0800

    Implement function call

--- reamber/algorithms/timing/utils/Snapper.py
+++ reamber/algorithms/timing/utils/Snapper.py
@@ -5,13 +5,27 @@ from typing import Iterable
 
 import numpy as np
 
+DEFAULT_DIVISIONS = (1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 16, 32, 64, 96)
+
+
+def snap(value: float,
+         divisions: Iterable[int] = DEFAULT_DIVISIONS) -> Fraction:
+    """ Snaps float value to closest division.
+
+    Args:
+        value: Value to snap
+        divisions: Divisions to accept
+    """
+    return Snapper(divisions=divisions).snap(value)
+
 
 class Snapper:
-    def __init__(
-        self,
-        divisions: Iterable[int] = (1, 2, 3, 4, 5, 6, 7,
-                                    8, 9, 12, 16, 32, 64, 96)
-    ):
+    def __init__(self, divisions: Iterable[int] = DEFAULT_DIVISIONS):
+        """ Initialize Snapper with defined divisions
+
+        Args:
+            divisions:
+        """
         divisions = np.asarray(divisions)
         max_slots = max(divisions)
 
@@ -32,7 +46,7 @@ class Snapper:
         ar = np.stack([ar, *np.indices(ar.shape)])[:, divisions - 1]
         self.ar = ar[:, ~np.isnan(ar[0])].T
 
-    def snap(self, snap_as_float: float) -> Fraction:
-        """ Convert snap in float to a Fraction """
-        closest = self.ar[np.argmin(np.abs(self.ar[:, 0] - snap_as_float))]
+    def snap(self, value: float) -> Fraction:
+        """ Snaps float value to closest division """
+        closest = self.ar[np.argmin(np.abs(self.ar[:, 0] - value))]
         return Fraction(int(closest[2]), int(closest[1] + 1))
