02566bc44930e408e703c5c915e975c5af6b155e
Evening <johnchangqi@hotmail.com>
2022-05-24 13:26:19 +0800

    Automatically reseat in not on measures

--- reamber/algorithms/timing/utils/from_bpm_changes_snap.py
+++ reamber/algorithms/timing/utils/from_bpm_changes_snap.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
+import logging
 from typing import List
 
 from reamber.algorithms.timing.utils.BpmChangeOffset import BpmChangeOffset
 from reamber.algorithms.timing.utils.BpmChangeSnap import BpmChangeSnap
+from reamber.algorithms.timing.utils.reseat_bpm_changes_snap import \
+    reseat_bpm_changes_snap
 
 
 def from_bpm_changes_snap(initial_offset: float,
@@ -19,7 +22,7 @@ def from_bpm_changes_snap(initial_offset: float,
 
     assert bpm_changes_snap[0].snap.measure == 0 and \
            bpm_changes_snap[0].snap.beat == 0 and \
-        f"The first bpm must be on Measure 0, Beat 0"
+           f"The first bpm must be on Measure 0, Beat 0"
     bpm_changes_offset = [
         BpmChangeOffset(bpm_changes_snap[0].bpm,
                         bpm_changes_snap[0].metronome,
@@ -31,8 +34,12 @@ def from_bpm_changes_snap(initial_offset: float,
     for active_bpm_change, bpm_change in zip(bpm_changes_snap[:-1],
                                              bpm_changes_snap[1:]):
         diff_snap = bpm_change.snap - active_bpm_change.snap
-        if diff_snap.beat == 0:
-            raise ValueError("All Bpm Points must be on measures.")
+        if diff_snap.beat != 0:
+            logging.warning("All Bpm Points must be on measures. Reseating")
+            return from_bpm_changes_snap(
+                initial_offset,
+                reseat_bpm_changes_snap(bpm_changes_snap)
+            )
 
         offset += diff_snap.offset(active_bpm_change)
         bpm_changes_offset.append(
