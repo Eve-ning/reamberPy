c16a2a784e35f990fdf513504045fd4d1daa81fe
Evening <johnchangqi@hotmail.com>
2022-06-04 13:36:05 +0800

    Move all tests to unit_tests

--- tests/unit_tests/algorithms/convert/test_to_bms.py
+++ tests/unit_tests/algorithms/convert/test_to_bms.py
@@ -1,21 +1,22 @@
+import pytest
+
 from reamber.algorithms.convert import O2JToBMS, OsuToBMS, QuaToBMS, SMToBMS
 from reamber.bms.BMSChannel import BMSChannel
-from tests.test.algorithms.convert.test_fixture import o2js, osu, qua, bms, sms
 
 
-def test_o2j(o2js):
+def test_o2j(o2j_mapset):
     bmss = O2JToBMS.convert(o2js)
-    bmss[0].write_file('out.bme', BMSChannel.BME)
+    bmss[0].write(BMSChannel.BME)
 
 def test_osu(osu):
     bms = OsuToBMS.convert(osu, move_right_by=1)
-    bms.write_file('out.bme', BMSChannel.BME)
+    bms.write(BMSChannel.BME)
 
 def test_qua(qua):
     bms = QuaToBMS.convert(qua, move_right_by=1)
-    bms.write_file('out.bme', BMSChannel.BME)
+    bms.write(BMSChannel.BME)
 
 def test_sm(sms):
     bmss = SMToBMS.convert(sms)
-    bmss[0].write_file('out.bme', BMSChannel.BME)
+    bmss[0].write(BMSChannel.BME)
 
---
+++ tests/unit_tests/conftest.py
@@ -0,0 +1,45 @@
+from pathlib import Path
+
+import pytest
+
+from reamber.o2jam import O2JMapSet
+from reamber.osu import OsuMap
+from reamber.quaver import QuaMap
+from reamber.sm import SMMapSet
+
+RSC_DIR = Path(__file__).parents[2] / 'rsc/maps'
+
+
+@pytest.fixture(scope='session')
+def osu_map():
+    return OsuMap.read_file((RSC_DIR / 'osu/Gravity.osu').as_posix())
+
+
+@pytest.fixture(scope='session')
+def qua_map():
+    return QuaMap.read_file((RSC_DIR / 'qua/CarryMeAway.qua').as_posix())
+
+
+@pytest.fixture(scope='session')
+def sm_mapset():
+    return SMMapSet.read_file((RSC_DIR / 'sm/Escapes.sm').as_posix())
+
+
+@pytest.fixture(scope='session')
+def sm_map():
+    return sm_mapset[0]
+
+
+@pytest.fixture(scope='session')
+def o2j_mapset():
+    return O2JMapSet.read_file((RSC_DIR / 'o2jam/o2ma178.ojn').as_posix())
+
+
+@pytest.fixture(scope='session')
+def o2j_map():
+    return o2j_mapset[0]
+
+
+@pytest.fixture(scope='session')
+def bms_coldbreath():
+    return RSC_DIR / 'bms/coldBreath.bme'
