7b8c39e051c0cc4c4ac45f5e6ab63b845aec000c
Evening <johnchangqi@hotmail.com>
2022-06-04 14:22:30 +0800

    Clean hit and holds

--- tests/unit_tests/base/test_hit.py
+++ tests/unit_tests/base/test_hit.py
@@ -1,24 +1,17 @@
-import unittest
-
 import pandas as pd
+import pytest
 
 from reamber.base import Hit
 
 
-class TestHit(unittest.TestCase):
-    """ Not much to test here since Hit is basically Note. """
-
-    def setUp(self) -> None:
-        self.hit = Hit(offset=1000, column=1)
-
-    # @profile
-    def test_type(self):
-        self.assertTrue(isinstance(self.hit.data, pd.Series))
-
-    def test_from_series(self):
-        hit = Hit.from_series(pd.Series(dict(offset=1000, column=1)))
-        self.assertEqual(self.hit, hit)
+@pytest.fixture
+def hit():
+    return Hit(offset=1000, column=1)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_type(hit):
+    assert isinstance(hit.data, pd.Series)
+
+
+def test_from_series(hit):
+    assert Hit.from_series(pd.Series(dict(offset=1000, column=1))), hit
--- tests/unit_tests/base/test_hold.py
+++ tests/unit_tests/base/test_hold.py
@@ -1,46 +1,42 @@
-import unittest
-
 import pandas as pd
 
 from reamber.base import Hold
 
 
-class TestHold(unittest.TestCase):
-    """ The purpose of this test is to test the architecture of Base. """
-
-    def setUp(self) -> None:
-        self.hold = Hold(offset=1000, column=1, length=1000)
-
-    # @profile
-    def test_type(self):
-        self.assertTrue(isinstance(self.hold.data, pd.Series))
-
-    def test_eq(self):
-        self.assertEqual(Hold(offset=1000, column=1, length=1000), self.hold)
-        self.assertNotEqual(Hold(offset=1000, column=1, length=2000), self.hold)
-
-    def test_length(self):
-        self.assertEqual(1000, self.hold.length)
-
-    def test_tail_offset(self):
-        self.assertEqual(2000, self.hold.tail_offset)
-
-    def test_deepcopy(self):
-        self.assertFalse(self.hold is Hold(offset=1000, column=1, length=1000))
-        self.assertFalse(self.hold is self.hold.deepcopy())
-        hold = self.hold
-        self.assertTrue(self.hold is hold)
-
-    def test_length_op(self):
-        self.assertEqual(1000, self.hold.length)
-        self.hold.length *= 2
-        self.assertEqual(2000, self.hold.length)
-        self.assertEqual(3000, self.hold.tail_offset)
-        # An odd occurrence, but we support negative lengths.
-        self.hold.length = -1000
-        self.assertEqual(-1000, self.hold.length)
-        self.assertEqual(0, self.hold.tail_offset)
+def hold():
+    return Hold(offset=1000, column=1, length=1000)
 
 
-if __name__ == '__main__':
-    unittest.main()
+def test_type(hold):
+    assert isinstance(hold.data, pd.Series)
+
+
+def test_eq(hold):
+    assert Hold(offset=1000, column=1, length=1000) == hold
+    assert Hold(offset=1000, column=1, length=2000) != hold
+
+
+def test_length(hold):
+    assert 1000 == hold.length
+
+
+def test_tail_offset(hold):
+    assert 2000 == hold.tail_offset
+
+
+def test_deepcopy(hold):
+    assert hold is not Hold(offset=1000, column=1, length=1000)
+    assert hold is not hold.deepcopy()
+    hold_copy = hold
+    assert hold_copy is hold
+
+
+def test_length_op(hold):
+    assert 1000 == hold.length
+    hold.length *= 2
+    assert 2000 == hold.length
+    assert 3000 == hold.tail_offset
+    # An odd occurrence, but we support negative lengths.
+    hold.length = -1000
+    assert -1000 == hold.length
+    assert 0 == hold.tail_offset
