54d3223939c33b30d75a4380f65a02f3f43605bc
Evening <johnchangqi@hotmail.com>
2022-07-01 23:03:51 +0800

    Remove io tests

--- tests/unit_tests/sm/test_map.py
+++ tests/unit_tests/sm/test_map.py
@@ -1,35 +1,18 @@
-from pathlib import Path
-
-import pytest
-
-from reamber.sm.SMMapSet import SMMapSet
-
-
-def test_read():
-    with open(MAP_PATH) as f:
-        content = f.readlines()
-
-
-def test_write(sm_mapset):
-    sm_mapset.write()
-
-
 def test_meta(sm_mapset):
-    sms = SMMapSet.read_file(MAP_PATH)
-    assert sms.title == "Escapes"
-    assert sms.subtitle == "subtitle"
-    assert sms.artist == "Draw the Emotional x Foreground Eclipse"
-    assert sms.title_translit == "titletranslit"
-    assert sms.subtitle_translit == "subtitletranslit"
-    assert sms.artist_translit == "artisttranslit"
-    assert sms.genre == "genre"
-    assert sms.credit == "credit"
-    assert sms.banner == "Escapes - bn.png"
-    assert sms.background == "Escapes - bg.jpg"
-    assert sms.lyrics_path == "lyricspath"
-    assert sms.cd_title == "CDTitle.gif"
-    assert sms.music == "Escapes.mp3"
-    assert sms.offset == -635
-    assert sms.sample_start == 68502
-    assert sms.sample_length == 26000
-    assert sms.selectable
+    assert sm_mapset.title == "Escapes"
+    assert sm_mapset.subtitle == "subtitle"
+    assert sm_mapset.artist == "Draw the Emotional x Foreground Eclipse"
+    assert sm_mapset.title_translit == "titletranslit"
+    assert sm_mapset.subtitle_translit == "subtitletranslit"
+    assert sm_mapset.artist_translit == "artisttranslit"
+    assert sm_mapset.genre == "genre"
+    assert sm_mapset.credit == "credit"
+    assert sm_mapset.banner == "Escapes - bn.png"
+    assert sm_mapset.background == "Escapes - bg.jpg"
+    assert sm_mapset.lyrics_path == "lyricspath"
+    assert sm_mapset.cd_title == "CDTitle.gif"
+    assert sm_mapset.music == "Escapes.mp3"
+    assert sm_mapset.offset == 635
+    assert sm_mapset.sample_start == 68502
+    assert sm_mapset.sample_length == 26000
+    assert sm_mapset.selectable
