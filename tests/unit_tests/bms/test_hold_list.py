7f449da76594250c853db95d81ec751f565f9370
Evening <johnchangqi@hotmail.com>
2022-07-03 21:52:59 +0800

    Minor Lint

--- tests/unit_tests/bms/test_hold_list.py
+++ tests/unit_tests/bms/test_hold_list.py
@@ -1,9 +1,11 @@
-import pandas as pd
-
 from reamber.bms.lists.notes import BMSHoldList
 
+
 def test_df_names(bms_map):
-    assert {'offset', 'column', 'length', 'sample'} == set(bms_map.holds.df.columns)
+    assert {'offset', 'column', 'length', 'sample'} == set(
+        bms_map.holds.df.columns)
+
 
 def test_empty(bms_map):
-    assert {'offset', 'column', 'length', 'sample'} == set(BMSHoldList([]).df.columns)
+    assert {'offset', 'column', 'length', 'sample'} == set(
+        BMSHoldList([]).df.columns)
