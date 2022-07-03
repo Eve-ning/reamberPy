bce72cbde0773d278bc69e4fe33627b4901c8209
Evening <johnchangqi@hotmail.com>
2022-07-03 21:53:50 +0800

    Drop redundant tests

--- tests/unit_tests/bms/test_map.py
+++ tests/unit_tests/bms/test_map.py
@@ -1,27 +1,14 @@
-import os
 from pathlib import Path
-
-import pytest
 
 from reamber.algorithms.playField import PlayField
 from reamber.algorithms.playField.parts import PFDrawBeatLines, PFDrawNotes, \
     PFDrawBpm
-from reamber.bms.lists.BMSBpmList import BMSBpmList
-from reamber.bms.lists.notes.BMSHitList import BMSHitList
-from reamber.bms.lists.notes.BMSHoldList import BMSHoldList
 
 THIS_DIR = Path(__file__).parent
 
 MAP_READ = THIS_DIR / 'searoad.bml'
 MAP_WRITE_EXP = THIS_DIR / 'map_write_expected.bme'
 MAP_WRITE = THIS_DIR / 'map_write.bme'
-
-
-# @profile
-def test_type(bms_map):
-    assert isinstance(bms_map.hits, BMSHitList)
-    assert isinstance(bms_map.holds, BMSHoldList)
-    assert isinstance(bms_map.bpms, BMSBpmList)
 
 
 def test_describe(bms_map):
@@ -32,24 +19,6 @@ def test_meta(bms_map):
     assert bms_map.title == b'searoad tracks =side blue= (LN-Applied)'
     assert bms_map.artist == b'sasakure.UK / obj:moya'
     assert bms_map.version == b'16'
-
-
-def test_deepcopy(bms_map):
-    m = bms_map.deepcopy()
-    assert m is not bms_map
-    test_meta(m)
-
-
-def test_stack_mutate(bms_map):
-    original = bms_map.hits[0].sample
-    bms_map.stack().sample += b'_'
-    assert original + b'_' == bms_map.hits[0].sample
-    with pytest.raises(TypeError): bms_map.stack().sample += 1
-
-
-def test_rate(bms_map):
-    assert bms_map.stack().offset.min() * 2 == bms_map.rate(
-        0.5).stack().offset.min()
 
 
 def test_draw(bms_map):
