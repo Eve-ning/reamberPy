1a77eccd86dde63c461573f27cdbb65c85b711c4
Evening <johnchangqi@hotmail.com>
2022-07-04 21:30:27 +0800

    Patch for cases of approximate matching

--- reamber/algorithms/timing/utils/reseat_bpm_changes_snap.py
+++ reamber/algorithms/timing/utils/reseat_bpm_changes_snap.py
@@ -6,11 +6,24 @@ from typing import List
 from reamber.algorithms.timing.utils.BpmChangeSnap import BpmChangeSnap
 from reamber.algorithms.timing.utils.snap import Snap
 
-THRES = 0
 
-
-def reseat_bpm_changes_snap(bcs_s: List[BpmChangeSnap]) -> List[BpmChangeSnap]:
+def reseat_bpm_changes_snap(
+    bcs_s: List[BpmChangeSnap],
+    extend_threshold: float = 0.001
+) -> List[BpmChangeSnap]:
     """ Force all bpm changes to be on metronome
+
+    Notes:
+        The case where extend is used.
+        BPM 60000, Offset 0     , Metronome 4
+        BPM 60000, Offset 4.0001, Metronome 4
+
+        Instead of adding a BPM on Offset 4, we simply change the first BPM.
+        This prevents the case of a super high bpm.
+
+    Args:
+        bcs_s: Bpm Changes to reseat
+        extend_threshold: Fraction of Measure to accept extending a late BPM
 
     Notes:
         1st BPM Change MUST be on Measure, Beat, Slot 0.
@@ -19,7 +32,7 @@ def reseat_bpm_changes_snap(bcs_s: List[BpmChangeSnap]) -> List[BpmChangeSnap]:
     bcs_s.sort(key=lambda x: x.snap)
 
     offset = 0
-    offsets = [0]
+    offsets = [0, ]
     for bcs_0, bcs_1 in zip(bcs_s[:-1], bcs_s[1:]):
         diff = (bcs_1.snap - bcs_0.snap).offset(bcs_0)
         offset += diff
@@ -38,7 +51,7 @@ def reseat_bpm_changes_snap(bcs_s: List[BpmChangeSnap]) -> List[BpmChangeSnap]:
         measure += measure_diff_quo
 
         # BCS_0 is guaranteed to be on a measure.
-        if measure_diff_rem > THRES:
+        if measure_diff_rem > extend_threshold:
             bcs = BpmChangeSnap(bcs_0.bpm / measure_diff_rem,
                                 bcs_0.metronome,
                                 Snap(measure, 0, bcs_0.metronome))
@@ -51,7 +64,21 @@ def reseat_bpm_changes_snap(bcs_s: List[BpmChangeSnap]) -> List[BpmChangeSnap]:
                 bcs_s.insert(i + 1, bcs)
                 offsets.insert(i + 1, offset)
 
+        # Extend case, see docstring
+        elif 0 < measure_diff_rem <= extend_threshold:
+            bcs = BpmChangeSnap(bcs_0.bpm / (measure_diff_rem + 1),
+                                bcs_0.metronome,
+                                Snap(measure - 1, 0, bcs_0.metronome))
+            offset = (measure_diff_quo - 1) * bcs_0.measure_length + offset_0
+            if (measure_diff_quo - 1) == 0:
+                bcs_s[i] = bcs
+                offsets[i] = offset
+            else:
+                bcs_s.insert(i + 1, bcs)
+                offsets.insert(i + 1, offset)
+
         bcs_s[i + 1].snap.measure = measure
         bcs_s[i + 1].snap.beat = 0
         i += 1
+
     return bcs_s
--- tests/unit_tests/timing/test_reseat_cases.py
+++ tests/unit_tests/timing/test_reseat_cases.py
@@ -44,3 +44,29 @@ def test_reseat(i, o):
         [BpmChangeSnap(60000, 4, Snap(0, x, 4)) for x in i]
     )
     assert bcs_s == expected_bcs_s
+
+
+def test_reseat_approx_late():
+    """ In the case where the measures aren't exact, we add a measure. """
+
+    tm = TimingMap.from_bpm_changes_snap(
+        0,
+        [BpmChangeSnap(60000, 4, Snap(0, 0, 4)),
+         BpmChangeSnap(60000, 4, Snap(1, 0.0001, 4)),
+         BpmChangeSnap(60000, 4, Snap(2, 0, 4))]
+    )
+    assert [bco.offset for bco in tm.bpm_changes_offset] == [0, 4.0001, 8]
+
+
+def test_reseat_approx_early():
+    """ In the case where the measures aren't exact, we add a measure. """
+
+    tm = TimingMap.from_bpm_changes_snap(
+        0,
+        [
+            BpmChangeSnap(60000, 4, Snap(0, 0, 4)),
+            BpmChangeSnap(60000, 4, Snap(0, 3.9999, 4)),
+            BpmChangeSnap(60000, 4, Snap(2, 0, 4))
+        ]
+    )
+    assert [bco.offset for bco in tm.bpm_changes_offset] == [0, 3.9999, 8]
