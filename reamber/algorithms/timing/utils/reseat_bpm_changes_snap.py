2822c4e97e024511a90db8662b97f48cd7e075ec
Evening <johnchangqi@hotmail.com>
2022-07-04 19:04:53 +0800

    Implement passing reseat

--- reamber/algorithms/timing/utils/reseat_bpm_changes_snap.py
+++ reamber/algorithms/timing/utils/reseat_bpm_changes_snap.py
@@ -1,9 +1,12 @@
 from __future__ import annotations
 
+from copy import deepcopy
 from typing import List
 
 from reamber.algorithms.timing.utils.BpmChangeSnap import BpmChangeSnap
 from reamber.algorithms.timing.utils.snap import Snap
+
+THRES = 0
 
 
 def reseat_bpm_changes_snap(bcs_s: List[BpmChangeSnap]) -> List[BpmChangeSnap]:
@@ -12,50 +15,43 @@ def reseat_bpm_changes_snap(bcs_s: List[BpmChangeSnap]) -> List[BpmChangeSnap]:
     Notes:
         1st BPM Change MUST be on Measure, Beat, Slot 0.
     """
+    bcs_s = deepcopy(bcs_s)
     bcs_s.sort(key=lambda x: x.snap)
 
-    assert bcs_s[0].snap.measure == 0 and \
-           bcs_s[0].snap.beat == 0, \
-        f"The first bpm must be on Measure 0, Beat 0."
+    offset = 0
+    offsets = [0]
+    for bcs_0, bcs_1 in zip(bcs_s[:-1], bcs_s[1:]):
+        diff = (bcs_1.snap - bcs_0.snap).offset(bcs_0)
+        offset += diff
+        offsets.append(offset)
 
-    new_bcs = [bcs_s[-1]]
-    for ix in reversed(range(len(bcs_s) - 1)):
-        parent_bc = bcs_s[ix]
-        child_bc = bcs_s[ix + 1]
+    i = 0
+    measure = 0
 
-        diff_snap = child_bc.snap - parent_bc.snap
+    while i != len(bcs_s) - 1:
+        bcs_0, bcs_1 = bcs_s[i], bcs_s[i + 1]
+        offset_0, offset_1 = offsets[i], offsets[i + 1]
+        offset_diff = offset_1 - offset_0
+        measure_diff = offset_diff / bcs_0.measure_length
+        measure_diff_quo = measure_diff // 1
+        measure_diff_rem = measure_diff % 1
+        measure += measure_diff_quo
 
-        # If it yields a perfect measure, we add and continue
-        if diff_snap.beat == 0:
-            new_bcs.insert(0, parent_bc)
-            continue
+        # BCS_0 is guaranteed to be on a measure.
+        if measure_diff_rem > THRES:
+            bcs = BpmChangeSnap(bcs_0.bpm / measure_diff_rem,
+                                bcs_0.metronome,
+                                Snap(measure, 0, bcs_0.metronome))
+            offset = measure_diff_quo * bcs_0.measure_length + offset_0
+            if measure_diff_quo == 0:
+                bcs_s[i] = bcs
+                offsets[i] = offset
+                measure += 1
+            else:
+                bcs_s.insert(i + 1, bcs)
+                offsets.insert(i + 1, offset)
 
-        # Check if they are of the same measure, see below for usage.
-        same_measure = child_bc.snap.measure == parent_bc.snap.measure
-
-        # If round up increases by a measure, we move everything after
-        if child_bc.snap.beat > 0:
-            for b in new_bcs[1:]:
-                b.snap.measure += 1
-            child_bc.snap.round_up()
-
-        new_bpm = parent_bc.bpm / (diff_snap.beat / parent_bc.metronome)
-
-        if same_measure:
-            # If in the same measure, we don't add a point
-            new_bcs.insert(0,
-                           BpmChangeSnap(new_bpm,
-                                         parent_bc.metronome,
-                                         parent_bc.snap)
-                           )
-        else:
-            new_bcs.insert(0,
-                           BpmChangeSnap(
-                               new_bpm,
-                               parent_bc.metronome,
-                               Snap(child_bc.snap.measure - 1, 0,
-                                    parent_bc.metronome))
-                           )
-            new_bcs.insert(0, parent_bc)
-
-    return new_bcs
+        bcs_s[i + 1].snap.measure = measure
+        bcs_s[i + 1].snap.beat = 0
+        i += 1
+    return bcs_s
