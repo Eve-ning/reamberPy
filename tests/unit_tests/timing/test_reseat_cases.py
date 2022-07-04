619a170df94347a657d2a0dfe0d2af2470102737
Evening <johnchangqi@hotmail.com>
2022-07-04 18:03:00 +0800

    Update test syntax and requirements

--- tests/unit_tests/timing/test_reseat_cases.py
+++ tests/unit_tests/timing/test_reseat_cases.py
@@ -9,35 +9,38 @@ from reamber.algorithms.timing.utils.snap import Snap
 Cases   0 1 2 3 4 5 6 7 8 9           0 1 2 3 4 5 6 7 8 9
 Normal  | - - - | - - - |     |-->|   | - - - | - - - |             
 >-|     | - - - o | - - |     |-->|   | - - - | | - - |             
-<-|     | - - | o - - - |     |-->|   | - - | | - - - |             
+<-|     | - - | o - - - |     |-->|   | - - | - - - | |             
 |->     | - - - | - - - o |   |-->|   | - - - | - - - | |            
-|-<     | - - - | - - | o     |-->|   | - - - | - - | |              
-<-<     | - - | o - - | o     |-->|   | - - | | - - | |              
->->     | - - - o | - - o |   |-->|   | - - - | | - - | |                
->-<     | - - - o | - | o     |-->|   | - - - | | - | |              
-<->     | - - | o - - - o |   |-->|   | - - | | - - - | |                
+|-<     | - - - | - - | o     |-->|   | - - - | - - |               
+<-<     | - - | - - - | o     |-->|   | - - | - - - |               
+>->     | - - - o | - - - |   |-->|   | - - - | | - - - |                
+>-<     | - - - o | - | o     |-->|   | - - - | | - |               
+<->     | - - | o - - - o |   |-->|   | - - | - - - | - |                
 """
 
 
 @pytest.mark.parametrize(
-    'bc_0',
-    (3, 4, 5),
-    ids=['<', '-', '>']
+    'i, o',
+    [
+        [(0, 4, 8), (0, 4, 8)],
+        [(0, 5, 8), (0, 4, 5, 8)],
+        [(0, 3, 8), (0, 3, 7, 8)],
+        [(0, 4, 9), (0, 4, 8, 9)],
+        [(0, 4, 7), (0, 4, 7)],
+        [(0, 3, 7), (0, 3, 7)],
+        [(0, 5, 9), (0, 4, 5, 9)],
+        [(0, 5, 7), (0, 4, 5, 7)],
+        [(0, 3, 9), (0, 3, 7, 9)],
+    ],
+    ids=["Normal", ">-|", "<-|", "|->", "|-<", "<-<", ">->", ">-<", "<->"]
 )
-@pytest.mark.parametrize(
-    'bc_1',
-    (7, 8, 9),
-    ids=['<', '-', '>']
-)
-def test_reseat(bc_0, bc_1):
+def test_reseat(i, o):
     """ In the case where the measures aren't exact, we add a measure. """
-    expected = sorted(list({0, 4, 8, bc_0, bc_1}))
-    bpms = 60000 / np.diff(expected) * 4
+    bpms = 60000 / np.diff(o) * 4
     expected_bcs_s = [BpmChangeSnap(bpm, 4, Snap(e, 0, 4))
                       for e, bpm in enumerate([*bpms, 60000])]
-    inp = [0, bc_0, bc_1]
-    tm = TimingMap.from_bpm_changes_snap(
-        0,
-        [BpmChangeSnap(60000, 4, Snap(0, x, 4)) for x in inp]
+
+    bcs_s = TimingMap.reseat_bpm_changes_snap(
+        [BpmChangeSnap(60000, 4, Snap(0, x, 4)) for x in i]
     )
-    assert tm.bpm_changes_snap() == expected_bcs_s
+    assert bcs_s == expected_bcs_s
