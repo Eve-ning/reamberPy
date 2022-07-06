82390c34f1db0d4918207bc00eab903e0ca3bd80
Evening <johnchangqi@hotmail.com>
2022-07-06 17:53:49 +0800

    Drop tests for sv package

--- tests/unit_tests/test_algorithms/test_gen/sv/test_init.py
+++
@@ -1,8 +0,0 @@
-from reamber.algorithms.generate.sv.SvSequence import SvSequence, SvObj
-
-
-def test_mixed_init():
-    # Quick Init Mixed
-    seq = SvSequence([100, (200, 2.0, True), (400, 3.0),
-                      SvObj(offset=800, multiplier=5.0, fixed=True)])
-    assert len(seq) == 4
--- tests/unit_tests/test_algorithms/test_gen/sv/test_rescale.py
+++
@@ -1,6 +0,0 @@
-from reamber.algorithms.generate.sv.SvSequence import SvSequence
-
-
-def test_rescale():
-    seq = SvSequence([100, 200, 60, 400])
-    seq.rescale(300, 800, inplace=True)
