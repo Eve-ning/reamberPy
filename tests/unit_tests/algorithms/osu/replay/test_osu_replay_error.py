79e56338d85000d83afea622a224003a84f9736f
Evening <johnchangqi@hotmail.com>
2022-07-07 15:01:17 +0800

    Fix bad relative pkl path

--- tests/unit_tests/algorithms/osu/replay/test_osu_replay_error.py
+++ tests/unit_tests/algorithms/osu/replay/test_osu_replay_error.py
@@ -1,16 +1,13 @@
 import pickle
-from collections import defaultdict
-from dataclasses import field
-
-import numpy as np
-from osrparse import Replay, ReplayEventMania, Mod, parse_replay_file
+from pathlib import Path
 
 from reamber.algorithms.osu.OsuReplayError import OsuReplayError
-from reamber.osu import OsuMap
 from tests.unit_tests.conftest import MAPS_DIR, REPS_OSU_DIR
 
 MAP_PATH = MAPS_DIR / "osu/MAGiCVLGiRL_ZVPH.osu"
 REPS_PATH = REPS_OSU_DIR / "MAGiCVLGiRL_ZVPH.osu/"
+
+PKL_PATH = Path(__file__).parent / "errors.pkl"
 
 
 def test_replay():
@@ -18,7 +15,7 @@ def test_replay():
         [r.as_posix() for r in REPS_PATH.glob("*.osr")],
         MAP_PATH.as_posix()
     ).errors()
-    with open("errors.pkl", "rb+") as f:
+    with open(PKL_PATH, "rb+") as f:
         errors_exp = pickle.load(f)
 
     for act, exp in zip(errors.errors, errors_exp.errors):
@@ -28,4 +25,3 @@ def test_replay():
         for ar_act, ar_exp in zip(act.releases.values(),
                                   exp.releases.values()):
             assert all(ar_act == ar_exp)
-
