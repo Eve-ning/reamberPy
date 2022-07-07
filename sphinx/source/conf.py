51f6cfe384441de5302b7b5d2090ced7ebf9da4d
Evening <johnchangqi@hotmail.com>
2022-07-07 15:25:19 +0800

    Add support for google docstring format

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -31,7 +31,7 @@ release = '0.1.6'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx_rtd_theme',
-              'matplotlib.sphinxext.plot_directive',]
+              'matplotlib.sphinxext.plot_directive','sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
