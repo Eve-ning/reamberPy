f9f23b232f2b8105e7a7bc551cc0798032630b33
Evening <johnchangqi@hotmail.com>
2022-07-07 20:58:24 +0800

    Deprecate snap_offsets

--- reamber/base/lists/BpmList.py
+++ reamber/base/lists/BpmList.py
@@ -34,42 +34,6 @@ class BpmList(TimedList[Item]):
             f"Offset {offset} does not have a Bpm Associated with it.")
         return bpms[ix]
 
-    def snap_offsets(self, nths: float = 1.0,
-                     last_offset: float = None) -> np.ndarray:
-        """ Gets all of the nth snap offsets
-
-        For example::
-
-            SEC     1   2   3   4   5   6   7   8   9   10  11  12  13  14  ...
-            BPM     15                          7.5                         ...
-            SNAP    4/4 1/4 2/4 3/4 4/4 1/4 2/4 4/4 1/8 2/8 3/8 4/8 5/8 6/8 ...
-            BEAT    1               2           3
-            nths=1  ^               ^           ^
-            nths=2  ^       ^       ^       ^   ^               ^
-            nths=4  ^   ^   ^   ^   ^   ^   ^   ^       ^       ^       ^
-            nths=8  ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^   ^   ^   ^   ^   ^   ^
-
-        * Note: 15 BPM = 1 Beat per 4 seconds, 7.5 = 1 Beat per 8 seconds
-
-        The `^` indicates what offsets will be returned.
-
-        Args:
-            nths: Specifies the beat's snap, 1 = "1st"s, 4 = "4th"s, 16 = "16th"s
-            last_offset: The last offset to consider, if None, it uses the last BPM
-        """
-        self_ = self.deepcopy()
-        if last_offset: self_ = self_.append(
-            Bpm(last_offset, bpm=0))  # BPM doesn't matter for the last.
-
-        offsets = []
-        for i, j in zip(self_[:-1], self_[1:]):
-            i: Bpm
-            j: Bpm
-            offset_diff = j.offset - i.offset
-            nth_diff = i.beat_length / nths
-            offsets.append(np.arange(0, offset_diff, nth_diff) + i.offset)
-        return np.concatenate(offsets)
-
     def to_timing_map(self) -> TimingMap:
         return TimingMap.from_bpm_changes_offset(
             bpm_changes_offset=[
