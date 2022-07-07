f99e89150ff7b1dd4f47c952e820fa5363567abd
Evening <johnchangqi@hotmail.com>
2022-07-07 15:06:35 +0800

    Fix hitsound copy test

--- tests/unit_tests/algorithms/osu/hitsound_copy/test_hitsound_copy.py
+++ tests/unit_tests/algorithms/osu/hitsound_copy/test_hitsound_copy.py
@@ -6,7 +6,6 @@ from reamber.osu import OsuMap
 THIS_DIR = Path(__file__).parent
 
 EXPECTED_PATH = THIS_DIR / 'expected.osu'
-WRITE_PATH = THIS_DIR / 'write.osu'
 
 M_FROM = OsuMap.read_file(THIS_DIR / 'hitsound_src.osu')
 M_TO = OsuMap.read_file(THIS_DIR / 'hitsound_target.osu')
@@ -14,9 +13,6 @@ M_TO = OsuMap.read_file(THIS_DIR / 'hitsound_target.osu')
 
 def test_hitsound_copy():
     m_out = hitsound_copy(M_FROM, M_TO)
-    m_out.write_file(WRITE_PATH)
     with open(EXPECTED_PATH) as f:
         expected = f.read()
-    with open(WRITE_PATH) as f:
-        actual = f.read()
-    assert expected == actual
+    assert expected == "\n".join(m_out.write())
