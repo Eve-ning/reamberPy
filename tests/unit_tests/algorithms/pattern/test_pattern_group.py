fa8a2cba22c30ffbeb872cf87c05013ef1d58031
Evening <johnchangqi@hotmail.com>
2022-07-08 23:48:32 +0800

    Add group testing

---
+++ tests/unit_tests/algorithms/pattern/test_pattern_group.py
@@ -0,0 +1,15 @@
+import pytest
+
+
+@pytest.mark.parametrize(
+    'v_window, avoid_jack, group_offsets',
+    [
+        [0, True, [[0, 0], [100, 100], [200, 200]]],
+        [100, False, [[0, 0, 100, 100], [200, 200]]],
+        [100, True, [[0, 0, 100], [100, 200, 200]]],
+    ]
+)
+def test_pattern_group(pattern, v_window, avoid_jack, group_offsets):
+    pgs = pattern.group(v_window, None, avoid_jack)
+    for e, g in enumerate(group_offsets):
+        assert all(pgs[e].offset == g), (pgs[e].offset, g)
