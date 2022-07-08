e2ecf7e8f054d27b7dfce4032c457a7bf8b3a77e
Evening <johnchangqi@hotmail.com>
2022-07-08 23:48:25 +0800

    Update for static method

--- tests/unit_tests/algorithms/pattern/test_pattern.py
+++ tests/unit_tests/algorithms/pattern/test_pattern.py
@@ -16,17 +16,14 @@ def test_pattern_from_nl(hit_list, hold_list, columns, offsets, types):
 
 
 def test_pattern_h_mask(pattern):
-    assert all(pattern.h_mask(1, 1) == [True, True, True, True, True, False])
-    assert all(pattern.h_mask(0, 1) == [True, True, True, False, False, False])
+    assert all(pattern.h_mask(pattern.df, 1, 1) ==
+               [True, True, True, True, True, False])
+    assert all(pattern.h_mask(pattern.df, 0, 1) ==
+               [True, True, True, False, False, False])
 
 
 def test_pattern_v_mask(pattern):
-    assert all(pattern.v_mask(100, 100, False) ==
+    assert all(pattern.v_mask(pattern.df, 100, 100, False) ==
                [False, False, True, True, True, True])
-    assert all(pattern.v_mask(100, 100, True) ==
+    assert all(pattern.v_mask(pattern.df, 100, 100, True) ==
                [False, False, True, True, False, True])
-
-
-def test_pattern_group(pattern):
-    pgs = pattern.group(0, None, True, True)
-    pass
