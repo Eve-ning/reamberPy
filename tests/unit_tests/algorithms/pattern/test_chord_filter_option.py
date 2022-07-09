d58953d3301ef8669eeafb4354614b7737c4de53
Evening <johnchangqi@hotmail.com>
2022-07-09 14:07:09 +0800

    Fix bool comparison incompatibility

--- tests/unit_tests/algorithms/pattern/test_chord_filter_option.py
+++ tests/unit_tests/algorithms/pattern/test_chord_filter_option.py
@@ -4,35 +4,35 @@ from reamber.algorithms.pattern.filters.PtnFilter import PtnFilterChord
 
 
 def test_chord():
-    assert (
+    assert np.all(
         PtnFilterChord.create(
             chord_sizes=[[1, 1, 1]], keys=2,
         ).ar == np.array([1, 1, 1])
-    ).all()
+    )
 
 
 def test_chord_and_higher():
-    assert (
+    assert np.all(
         PtnFilterChord.create(
             chord_sizes=[[1, 1]], keys=2,
             options=PtnFilterChord.Option.AND_HIGHER
         ).ar == np.array([[1, 1], [1, 2], [2, 1], [2, 2]])
-    ).all()
+    )
 
 
 def test_chord_and_lower():
-    assert (
+    assert np.all(
         PtnFilterChord.create(
             chord_sizes=[[2, 2]], keys=2,
             options=PtnFilterChord.Option.AND_LOWER
         ).ar == np.array([[1, 1], [1, 2], [2, 1], [2, 2]])
-    ).all()
+    )
 
 
 def test_chord_any_order():
-    assert (
+    assert np.all(
         PtnFilterChord.create(
             chord_sizes=[[2, 1, 1]], keys=2,
             options=PtnFilterChord.Option.ANY_ORDER
         ).ar == np.array([[1, 1, 2], [1, 2, 1], [2, 1, 1]])
-    ).all()
+    )
