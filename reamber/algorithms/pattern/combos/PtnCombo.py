127c67db95fb44f40fd13be49cc94aa3f813538d
Evening <johnchangqi@hotmail.com>
2022-07-09 12:16:06 +0800

    Trim combinations function

--- reamber/algorithms/pattern/combos/PtnCombo.py
+++ reamber/algorithms/pattern/combos/PtnCombo.py
@@ -4,6 +4,7 @@ from dataclasses import dataclass, field
 from typing import List, Callable
 
 import numpy as np
+from numpy.lib.stride_tricks import sliding_window_view
 
 from reamber.algorithms.pattern.combos._PtnCChordStream import _PtnCChordStream
 from reamber.algorithms.pattern.combos._PtnCJack import _PtnCJack
@@ -53,100 +54,30 @@ class PtnCombo(_PtnCChordStream,
         # L       R
         # 0 1 2 3 4 5 ...
         for left_ix, right_ix in zip(
-            range(0, len(self.groups) - size),  # [0, Groups - Size]
-            range(size, len(self.groups))  # [Size, Groups]
+            range(0, len(self.groups) - size + 1),  # [0, Groups - Size]
+            range(size, len(self.groups) + 1)  # [Size, Groups]
         ):
             chunk = self.groups[left_ix:right_ix]
 
             if (
                 chord_filter is None or
-                chord_filter(np.asarray([i.shape[0] for i in chunk]))
+                chord_filter(np.array([i.shape[0] for i in chunk]))
             ):
-                chunks.append(chunk)
+                chunks.append([df.to_records(index=False) for df in chunk])
 
-        dt = np.dtype([*[(f'column{i}', np.int8) for i in range(size)],
-                       *[(f'offset{i}', np.float_) for i in range(size)],
-                       *[(f'type{i}', object) for i in range(size)]])
-
-        """ We loop through the chunks here, finding all permutations of each 
-        chunk
-        
-        e.g.
-        [0 1][3 4] -> [0][3] + [1][3] + [0][4] + [1][4]
-        
-        Note that this can scale up for size > 2.
-        
-        e.g.
-        [0][1][3 4] -> [0][1][3] + [0][1][4] ---[makeSize2 == True]
-            ---> [0][1] + [1][3] + [0][1] + [0][4]
-         
-        By allowing makeSize2, this algorithm will extract all 2-permutations. 
-        
-        At this point is where the filter will take place.
-        Depending on size specified, the filter argument will differ.
-        """
         combo_list: List = []
 
         for chunk in chunks:
-
             # This gets all combinations of the list of groups in the chunk
             combos = np.asarray(np.meshgrid(*chunk)).T.reshape(-1, size)
-
-            # This uses the combo filter to remove all unwanted sequences.
             if combo_filter: combos = combos[combo_filter(combos['column'])]
             if type_filter:  combos = combos[type_filter(combos['type'])]
 
-            """ Here we allocated an empty array to drop our data in. """
-            np_combo = np.empty(len(combos), dtype=dt)
+            combo_list.append(combos)
 
-            for i, combo in enumerate(combos):
-                for j, col, offset, type_ in \
-                    zip(range(size), combo['column'], combo['offset'],
-                        combo['type']):
-                    np_combo[i][f'column{j}'] = col
-                    np_combo[i][f'offset{j}'] = offset
-                    np_combo[i][f'type{j}'] = type_
-
-            combo_list.append(np_combo)
-
-        """ Outputs
-        
-        Let's say we have 4 original groups, size 3. [0][1 2][3][4]
-        
-        Reviewing how the algorithm is done.
-        
-        [0][1 2][3][4] -perm> [0][1][3] + [0][2][3] then [1][3][4] + [2][3][4] 
-        
-        If not flatten, we'll get all groups raw.
-        [[0][1][3], [0][2][3]],[[1][3][4], [2][3][4]]
-        <  From [0][1 2][3]  > <  From [1 2][3][4]  >
-        
-        If flatten
-        [0][1][3], [0][2][3], [1][3][4], [2][3][4]
-        
-        If flatten and makeSize2
-        [0][1], [1][3], [0][2], [2][3], [1][3], [3][4], [2][3], [3][4]
-        <  [0][1][3]  > <  [0][2][3]  > <  [1][3][4]  > <  [2][3][4]  > 
-        
-        If not flatten and makeSize2 will just return raw.
-        """
-
-        if not make_size2:
-            return np.asarray(
-                [i for j in combo_list for i in j]) if flatten else combo_list
-        else:
-            # This will make pairs out of (>2)-size combos by
-            # iterating through the combo pairs.
+        if make_size2:
             ar = np.asarray([i for j in combo_list for i in j])
-
-            # Algo not required for size 2.
-            if size == 2: return ar
-
-            s = [ar[[f'column{i}', f'column{i + 1}',
-                     f'offset{i}', f'offset{i + 1}',
-                     f'type{i}', f'type{i + 1}']] for i in range(size - 1)]
-
-            # Numpy doesn't allow hstack if names are inconsistent.
-            for i in s[1:]: i.dtype.names = ['column0', 'column1', 'offset0',
-                                             'offset1', 'type0', 'type1']
-            return np.hstack(s)
+            return sliding_window_view(ar, [ar.shape[0], 2])
+        else:
+            return np.array([i for j in combo_list for i in j]) \
+                if flatten else combo_list
