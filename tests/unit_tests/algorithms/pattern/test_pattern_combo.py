d14d86c530e52dd09cef2fbdf132f8e93502d5b2
Evening <johnchangqi@hotmail.com>
2022-07-09 13:22:38 +0800

    Add test for combo

--- tests/unit_tests/algorithms/pattern/test_pattern_combo.py
+++ tests/unit_tests/algorithms/pattern/test_pattern_combo.py
@@ -1,7 +1,17 @@
 import pytest
 
-from reamber.algorithms.pattern import Pattern
+from reamber.algorithms.pattern.combos import PtnCombo
 
 
-def test_pattern_combo(pattern):
-    pattern.group()
+@pytest.mark.parametrize(
+    'size, shapes',
+    [
+        [2, [4, 4, 2]],
+        [3, [16, 8]],
+        [4, [24]],
+    ]
+)
+def test_pattern_combo(size, shapes, pattern):
+    combos = PtnCombo(pattern.group()).combinations(size=size, make_size2=True)
+    for shape, combo in zip(shapes, combos):
+        assert combo.shape[0] == shape
