4ea9d6af64b6b5f68f510db1caf221576e17babf
Evening <johnchangqi@hotmail.com>
2022-07-09 18:06:30 +0800

    Modify to use all PF elements

--- tests/unit_tests/algorithms/playField/test_playfield.py
+++ tests/unit_tests/algorithms/playField/test_playfield.py
@@ -12,8 +12,8 @@ from reamber.base.Map import Map
     'map',
     ['osu_map']  # , 'qua_map', 'sm_map', 'o2j_map', 'bms_map']
 )
-def test_draw_pf(map: Map, request):
-    map = request.getfixturevalue(map)
+def test_draw_from_combo(map_str: str, request):
+    map: Map = request.getfixturevalue(map_str)
     ptn = Pattern.from_note_lists([map.hits, map.holds])
     grp = ptn.group(h_window=None, v_window=50, avoid_jack=True)
 
@@ -29,19 +29,15 @@ def test_draw_pf(map: Map, request):
                 keys=keys, and_lower=True), axis=0)
         ) +
         PFDrawLines.from_combo(
-            **PFDrawLines.Colors.BLUE,
-            keys=keys,
-            combo=np.concatenate(PtnCombo(grp).template_chord_stream(
-                primary=2, secondary=1,
-                keys=keys, and_lower=True), axis=0
-            )
-        ) +
-        PFDrawLines.from_combo(
             **PFDrawLines.Colors.PURPLE,
             keys=keys,
             combo=np.concatenate(PtnCombo(grp).template_jacks(
                 minimum_length=2, keys=keys), axis=0)
-        )
+        ) +
+        PFDrawNotes() +
+        PFDrawBpm() +
+        PFDrawBeatLines() +
+        PFDrawColumnLines()
     )
 
-    pf.export_fold(max_height=1000).save("out.png")
+    pf.export_fold(max_height=1000)
