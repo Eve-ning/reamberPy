b27bd60cefc19e9982a8d222a92a625874484f0b
Evening <johnchangqi@hotmail.com>
2022-07-13 11:05:30 +0800

    Complete Inverse foundation impl.

--- reamber/algorithms/generate/inverse/inverse.py
+++ reamber/algorithms/generate/inverse/inverse.py
@@ -1,70 +1,53 @@
 from dataclasses import dataclass, field
-from typing import Type, TypeVar
+from typing import TypeVar, Callable
 
 import numpy as np
 import pandas as pd
 
 from reamber.base.Map import Map
-from reamber.base.lists.notes.HitList import HitList
-from reamber.base.lists.notes.HoldList import HoldList
-from reamber.osu.OsuMap import OsuMap
-from tests.conftest import MAPS_DIR
 
 MapType = TypeVar('MapType', bound=Map)
 
 
 class LNHeadHandler:
     @staticmethod
-    def to_next(holds: list, offset, column, diff_next, *_):
-        holds += dict(offset=offset, column=column, length=diff_next)
+    def to_next(holds: list, offset, column, diff_next, **_):
+        holds.append(dict(offset=offset, column=column, length=diff_next))
 
     @staticmethod
-    def default(holds: list, offset, column, length):
-        holds += dict(offset=offset, column=column, length=length)
+    def default(holds: list, offset, column, length, **_):
+        holds.append(dict(offset=offset, column=column, length=length))
 
 
 class LNTailHandler:
     @staticmethod
-    def to_next(holds: list, offset, column, diff, *_):
-        holds += dict(offset=offset, column=column, length=diff)
+    def to_next(holds: list, offset, column, diff, **_):
+        holds.append(dict(offset=offset, column=column, length=diff))
 
     @staticmethod
-    def default(*_):
+    def default(**_):
         ...
 
 
 class HitHandler:
     @staticmethod
-    def to_next(offset, column, diff, *_):
-        return dict(offset=offset, column=column, length=diff)
+    def to_next(holds: list, offset, column, diff, **_):
+        holds.append(dict(offset=offset, column=column, length=diff))
 
     @staticmethod
-    def default(*_):
-        ...
+    def default(hits: list, offset, column, **_):
+        hits.append(dict(offset=offset, column=column))
+
 
 @dataclass
 class Inverse:
+    ln_tail_handler: Callable
+    ln_head_handler: Callable
+    hit_handler: Callable
     hits: list = field(default_factory=list, init=False)
     holds: list = field(default_factory=list, init=False)
 
-    def ln_tail_handler(self, offset, column, length, diff):
-        pass
-
-    def ln_head_handler(self, offset, column, length, diff, diff_next):
-        self.holds.append(
-            dict(offset=offset, column=column, length=diff_next)
-        )
-
-    def hit_handler(self, offset, column, diff):
-        if np.isnan(diff):
-            self.hits.append(dict(offset=offset, column=column))
-        else:
-            self.holds.append(dict(offset=offset, column=column, length=diff))
-
-    def invert(self,
-               m: MapType,
-               HoldListType: Type[HoldList],
-               HitListType: Type[HitList]) -> MapType:
+    def invert(self, m: MapType, ) -> MapType:
         df_hits = m.hits.df.loc[:, ['offset', 'column']].assign(is_ln=False)
         df_holds = (
             m.holds.df.loc[:, ['offset', 'column', 'length']]
@@ -89,19 +72,24 @@ class Inverse:
 
             for offset, column, is_ln, length, diff, diff_next in \
                 dfg.itertuples(index=False):
+                kwargs = dict(
+                    hits=self.hits, holds=self.holds,
+                    offset=offset, column=column, is_ln=is_ln,
+                    length=length, diff=diff, diff_next=diff_next
+                )
+                is_last_note = np.isnan(diff)
                 if is_ln_head and is_ln:
-                    self.ln_tail_handler(offset, column, length, diff)
+                    self.ln_tail_handler(**kwargs) if is_last_note \
+                        else LNTailHandler.default(**kwargs)
                 elif is_ln:
-                    self.ln_head_handler(offset, column, length, diff,
-                                         diff_next)
+                    self.ln_head_handler(**kwargs) if is_last_note \
+                        else LNHeadHandler.default(**kwargs)
                 else:
-                    self.hit_handler(offset, column, diff)
+                    self.hit_handler(**kwargs) if is_last_note \
+                        else HitHandler.default(**kwargs)
 
                 is_ln_head = is_ln
 
-        m.holds = HoldListType.from_dict(self.holds)
-        m.hits = HitListType.from_dict(self.hits)
+        m.holds = type(m.holds).from_dict(self.holds)
+        m.hits = type(m.hits).from_dict(self.hits)
         return m
-
-
-Inverse().invert(OsuMap.read_file(MAPS_DIR / 'osu/Stella.osu'))
