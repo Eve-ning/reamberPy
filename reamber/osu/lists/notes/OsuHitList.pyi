2ba8a4faa9b9cedbc168cb13d4c6e4d2a9f22f15
Evening <johnchangqi@hotmail.com>
2022-07-13 11:05:09 +0800

    Minor fix to missing inherit in stub

--- reamber/osu/lists/notes/OsuHitList.pyi
+++ reamber/osu/lists/notes/OsuHitList.pyi
@@ -2,11 +2,12 @@ from __future__ import annotations
 
 from typing import List
 
+from reamber.base.lists.notes.HitList import HitList
 from reamber.osu.OsuHit import OsuHit
 from reamber.osu.lists.notes.OsuNoteList import OsuNoteList
 
 
-class OsuHitList(OsuNoteList[OsuHit]):
+class OsuHitList(OsuNoteList[OsuHit], HitList[OsuHit]):
 
     @staticmethod
     def read(strings: List[str], keys: int) -> OsuHitList: ...
