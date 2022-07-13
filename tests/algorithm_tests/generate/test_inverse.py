019cdf1880a3b4ca71900f5cdb28dd30b854d86b
Evening <johnchangqi@hotmail.com>
2022-07-13 17:27:53 +0800

    Simplify test_inverse

--- tests/algorithm_tests/generate/test_inverse.py
+++ tests/algorithm_tests/generate/test_inverse.py
@@ -1,8 +1,7 @@
 import numpy as np
 import pytest
 
-from reamber.algorithms.generate.inverse.inverse import LNHeadHandler, \
-    LNTailHandler, HitHandler, Inverse
+from reamber.algorithms.generate import inverse
 from reamber.base.Map import Map
 from reamber.base.lists.notes.HitList import HitList
 from reamber.base.lists.notes.HoldList import HoldList
@@ -17,8 +16,12 @@ MIN_DIST = GAP + MINILN_AS_HIT
     'hit_offsets, hold_offsets, hold_lengths, '
     'exp_hit_offsets, exp_hold_offsets, exp_hold_lengths',
     [
-        ([0, MIN_DIST], [], [], [MIN_DIST], [0], [MINILN_AS_HIT]),
-        ([0, MIN_DIST - 1], [], [], [0, MIN_DIST - 1], [], []),
+        ([0, MIN_DIST], [], [],
+         [MIN_DIST], [0], [MINILN_AS_HIT]),
+        ([0, MIN_DIST - 1], [], [],
+         [0, MIN_DIST - 1], [], []),
+        ([], [0, MIN_DIST], [100, 100],
+         [], [0, MIN_DIST], [MINILN_AS_HIT, 100]),
     ]
 )
 def test_inverse_hit(hit_offsets, hold_offsets, hold_lengths,
@@ -27,12 +30,7 @@ def test_inverse_hit(hit_offsets, hold_offsets, hold_lengths,
     m.hits = HitList.from_dict({'offset': hit_offsets, })
     m.holds = HoldList.from_dict({'offset': hold_offsets,
                                   'length': hold_lengths})
-    inverser = Inverse(
-        ln_head_handler=LNHeadHandler.default,
-        ln_tail_handler=LNTailHandler.default,
-        hit_handler=HitHandler.to_next
-    )
-    m_out = inverser.invert(m, GAP, MINILN_AS_HIT)
+    m_out = inverse(m, GAP, MINILN_AS_HIT)
     assert np.all(m_out.hits.offset == exp_hit_offsets)
     assert np.all(m_out.holds.offset == exp_hold_offsets)
     assert np.all(m_out.holds.length == exp_hold_lengths)
