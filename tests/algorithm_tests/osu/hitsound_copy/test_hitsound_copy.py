4f2e410534fed2d1ef3951eaffc92b5b479912a9
Evening <johnchangqi@hotmail.com>
2023-05-10 15:35:03 +0800

    Change test case for no ln

--- tests/algorithm_tests/osu/hitsound_copy/test_hitsound_copy.py
+++ tests/algorithm_tests/osu/hitsound_copy/test_hitsound_copy.py
@@ -4,6 +4,7 @@ from pandas._testing import assert_frame_equal
 
 from reamber.algorithms.osu.hitsound_copy import hitsound_copy
 from reamber.osu import OsuMap
+from reamber.osu.lists.notes import OsuHoldList
 
 THIS_DIR = Path(__file__).parent
 
@@ -37,12 +38,7 @@ def test_hitsound_copy_nolns():
     target = OsuMap.read_file(THIS_DIR / 'target.osu')
 
     result = hitsound_copy(source, target)
-
-    assert_frame_equal(
-        result.holds[['offset', 'column', 'length']].df.sort_values(['offset', 'column']).reset_index(drop=True),
-        target.holds[['offset', 'column', 'length']].df.sort_values(['offset', 'column']).reset_index(drop=True),
-        check_like=True
-    )
+    target.holds = OsuHoldList([])
 
     assert_frame_equal(
         result.hits[['offset', 'column']].df.sort_values(['offset', 'column']).reset_index(drop=True),
