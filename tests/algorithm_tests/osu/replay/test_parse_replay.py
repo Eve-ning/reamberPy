a52f559efc9581b1209b8f05aa3c81fc78331563
Evening <johnchangqi@hotmail.com>
2023-05-22 17:42:59 +0800

    Improve robustness of test of parse_replay

--- tests/algorithm_tests/osu/replay/test_parse_replay.py
+++ tests/algorithm_tests/osu/replay/test_parse_replay.py
@@ -9,16 +9,14 @@ from tests.conftest import MAPS_DIR, REPS_OSU_DIR
 
 MAP_PATH = MAPS_DIR / "osu/MAGiCVLGiRL_ZVPH.osu"
 REPS_PATH = list((REPS_OSU_DIR / "MAGiCVLGiRL_ZVPH.osu/").glob("*.osr"))[:2]
+N_REPS = len(REPS_PATH)
 PKL_PATH = Path(__file__).parent / "errors.pkl"
 osu = OsuMap.read_file(MAP_PATH.as_posix())
 
 
 def test_parse_replay_action_osr():
-    df_actions = parse_replay_actions(
-        replay=REPS_PATH[0],
-        keys=4,
-        src='file'
-    )
+    print(REPS_PATH)
+    df_actions = parse_replay_actions(replay=REPS_PATH[0], keys=4, src='file')
     assert isinstance(df_actions, pd.DataFrame)
 
 
@@ -27,7 +25,10 @@ def test_parse_replays_error_osr():
         {r.as_posix(): r.as_posix() for r in REPS_PATH},
         osu=osu, src="file"
     )
-    assert isinstance(df_errors, pd.DataFrame)
+    cat_counts = df_errors.category.value_counts()
+    assert cat_counts['Hit'] == len(osu.hits) * N_REPS
+    assert cat_counts['Hold Head'] == len(osu.holds) * N_REPS
+    assert cat_counts['Hold Tail'] == len(osu.holds) * N_REPS
 
 
 def test_parse_replays_error_osr_infer():
@@ -35,7 +36,10 @@ def test_parse_replays_error_osr_infer():
         {r.as_posix(): r.as_posix() for r in REPS_PATH},
         osu=osu, src="infer"
     )
-    assert isinstance(df_errors, pd.DataFrame)
+    cat_counts = df_errors.category.value_counts()
+    assert cat_counts['Hit'] == len(osu.hits) * N_REPS
+    assert cat_counts['Hold Head'] == len(osu.holds) * N_REPS
+    assert cat_counts['Hold Tail'] == len(osu.holds) * N_REPS
 
 
 def test_parse_replays_error_api():
@@ -43,7 +47,10 @@ def test_parse_replays_error_api():
         data = json.load(f)
 
     df_errors = parse_replays_error({'rep1': data['content']}, osu=osu, src='api')
-    assert isinstance(df_errors, pd.DataFrame)
+    cat_counts = df_errors.category.value_counts()
+    assert cat_counts['Hit'] == len(osu.hits)
+    assert cat_counts['Hold Head'] == len(osu.holds)
+    assert cat_counts['Hold Tail'] == len(osu.holds)
 
 
 def test_parse_replays_error_api_infer():
@@ -51,4 +58,7 @@ def test_parse_replays_error_api_infer():
         data = json.load(f)
 
     df_errors = parse_replays_error({'rep1': data['content']}, osu=osu, src='infer')
-    assert isinstance(df_errors, pd.DataFrame)
+    cat_counts = df_errors.category.value_counts()
+    assert cat_counts['Hit'] == len(osu.hits)
+    assert cat_counts['Hold Head'] == len(osu.holds)
+    assert cat_counts['Hold Tail'] == len(osu.holds)
